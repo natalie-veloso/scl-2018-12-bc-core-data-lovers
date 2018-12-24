c3db6b4c520a0ec7314b4b6b6795829b2f30243d
GloriaMoraga <silmeriel@gail.com>
2018-12-24 15:50:05 -0300

    mostrando imagen y nombre

--- src/data.js
+++ src/data.js
@@ -1,16 +1,21 @@
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-const data = POKEMON.pokemon; //guardo en una constante el arreglo de objetos con los pokemones.
-//console.log(data);
 
-// función donde recorreré el arreglo de objetos para acceder a su propiedad name
-const nombrePokemones = (data) => {
+window.poke = {
+  filter: (textSearch) => {
+const data = POKEMON.pokemon; 
 
-  let nombres = []; // arreglo vacío donde pushearé los nombres nuevos.
-  for (let i = 0; i < data.length; i++) {
-    nombres.push(data[i].name)
+
+let nombres = []; // arreglo vacío donde pushearé los nombres nuevos.
+for (let i = 0; i < data.length; i++) {
+
+  if (data[i].name === textSearch){
+    nombres.push(data[i].name, data[i].img, data[i].weaknesses)
   }
-  return nombres; //retorno el arreglo de nombres para que pueda tomarlo desde el archivo main.js y hacer la visualización de datos con el DOM
 }
+  return nombres;
+  },
 
-window.nombrePokemones = nombrePokemones;
+
+
+};
+
+
--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
-const resultadoNombres = nombrePokemones(data); // guardo en la constante el resultado de mi función nombrePokemones (lo que está retornando)
-//console.log(resultadoNombres);
+// const resultadoNombres = nombrePokemones(data); // guardo en la constante el resultado de mi función nombrePokemones (lo que está retornando)
+// //console.log(resultadoNombres);
 
-// evento click del botón
+// // evento click del botón
+//   document.getElementById('btnSearch').addEventListener('click',
+//     (evento) => {evento.preventDefault();
+//   //document.getElementById('root').innerHTML = ''; // limpio el div cada vez que se hace click
+// let textSearch = document.getElementById('search').value;
+
+
+
+//   for (let i = 0; i < resultadoNombres[i].length; i++) {
+//   if( resultadoNombres[i] === textSearch)
+//   {
+//     document.getElementById('root').innerHTML += '<p>' + resultadoNombres + '</p>'; // imprimo en el HTML cada nombre que está dentro de cada posición del arreglo.
+//   }
+// }
+// });
+
+
+// tratando nuevo
+
+// // evento click del botón
   document.getElementById('btnSearch').addEventListener('click',
     (evento) => {evento.preventDefault();
-  //document.getElementById('root').innerHTML = ''; // limpio el div cada vez que se hace click
 let textSearch = document.getElementById('search').value;
+let resultado = poke.filter(textSearch); 
+document.getElementById('root').innerHTML += '<p>' + resultado[0] + '</p>' + '<img src="'+resultado[1]+'">'; 
 
-
-
-  for (let i = 0; i < resultadoNombres.length; i++) {
-  if( resultadoNombres[i] === textSearch){
-    document.getElementById('root').innerHTML += '<p>' + resultadoNombres[i] + '</p>'; // imprimo en el HTML cada nombre que está dentro de cada posición del arreglo.
-  }
-}
 });
