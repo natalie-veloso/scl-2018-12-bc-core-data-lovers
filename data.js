13bc676c39fee8becd09bb2d2747e784c5bc5ff8
GloriaMoraga <silmeriel@gail.com>
2018-12-26 22:47:19 -0300

    organizando funciones en datajs

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 
 window.poke = {
-  filter: (textSearch) => {
+  processData: (textSearch) => {
 const data = POKEMON.pokemon; 
 
 
@@ -12,9 +12,37 @@ for (let i = 0; i < data.length; i++) {
   }
 }
   return nombres;
+
+  // Mostrar la data en una interfaz: puede ser un card, una tabla, una lista, etc.
+  
+  },
+
+  filterData:(data, condition) => {
+
+  // esta función filter o filtrar recibiría la data,
+  //  y nos retornaría aquellos datos que sí cumplan con la condición.
+
+},
+
+
+  sortData:(data, sortBy, sortOrder) => {
+
+    // esta función sort u ordenar recibe tres parámetros.
+    //  El primer parámetro, data, nos entrega los datos. 
+    //  El segundo parámetro, sortBy, nos dice con respecto
+    //   a cuál de los campos de la data se quiere ordenar. 
+    //   El tercer parámetro, sortOrder, indica si se quiere 
+    //   ordenar de manera ascendente o descendente.
   },
 
 
+  computeStats:(data) => {
+
+    // la función compute o calcular, nos permitirá hacer 
+    // cálculos estadísticos básicos para ser mostrados de 
+    // acuerdo a la data proporcionada.
+
+  },
 
 };
 
