fbaa98d6407e828216b34594c4394e3b19215464
GloriaMoraga <silmeriel@gail.com>
2019-01-03 17:12:53 -0300

    añadiendo select y scroll

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,30 @@
-require('../src/data.js');
+assert = require('assert');
+describe('poke', () => {
 
+  it('debería ser un objeto', () => {
+    assert.equal(typeof poke, 'object');
+  });
+});
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('poke.processData', () => {
+  it('debería ser una función', () => {
+    assert.equal(typeof poke.processData, 'function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+
+  it('debería retornar "Bulbasaur" para "Bulbasaur"', () => {
+    assert.equal(poke.processData ('Bulbasaur'), 'Bulbasaur');
+  });
+});
+
+
+
+describe('poke.completeData', () => {
+  it('debería ser una función', () => {
+    assert.equal(typeof poke.processData, 'function');
+  });
+
+  it('debería retornar "Bulbasaur" para "Bulbasaur"', () => {
+    assert.equal(poke.processData ('Bulbasaur'), 'Bulbasaur');
   });
 });
