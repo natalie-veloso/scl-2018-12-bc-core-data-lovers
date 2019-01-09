29e49f47555486cf1cbabda2be09790f64f34210
GloriaMoraga <silmeriel@gail.com>
2019-01-09 10:18:16 -0300

    test funcionando con errores

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,13 +1,14 @@
-// global.window = global
-// global.chai=require("chai");
-// const require('../src/data.js');
-// require('../src/data/pokemon/pokemon.js');
+global.window=global  
+global.chai=require("chai");
+global.expect=require("chai").expect;
+global.expect=require("chai").assert;
+require('../src/data/pokemon/pokemon.js')
+require('../src/data.js');
 
-//import processData from '../src/data.js';
-require('../src/data/pokemon/pokemon.js');
 const dataTest = window.POKEMON.pokemon;
 
 const assert = require("assert"); 
+
 describe('poke', () => {
 
   it('debería ser un objeto', () => {
@@ -17,7 +18,7 @@ describe('poke', () => {
 
 
 
-describe('poke.processData', () => {
+describe('processData', () => {
   it('debería ser una función', () => {
     assert.equal(typeof window.poke.processData, 'function');
   });
@@ -28,16 +29,27 @@ describe('poke.processData', () => {
   });
 });
 
+describe('filterData', () => {
+  it('debería ser una función', () => {
+    assert.equal(typeof window.poke.filterData, 'function');
+  });
 
+
+  it('Debería retornar el primer pokemon de tipo planta', () => {
+    let pokeGrass= window.poke.filterData(dataTest,'Planta')[0].type
+    
+    assert.equal(pokeGrass, 'Bulbasaur')
+     ;});
+
+  });
 
 describe('poke.completeData', () => {
   it('debería ser una función', () => {
-    assert.equal(typeof window.poke.processData, 'function');
+    assert.equal(typeof window.poke.completeData, 'function');
   });
 
   it('debería retornar "Bulbasaur" para "Bulbasaur"', () => {
-    assert.equal(window.poke.processData ('Bulbasaur'), 'Bulbasaur');
+    assert.equal(window.poke.completeData ('Bulbasaur'), 'Bulbasaur');
   });
 });
 
-dataTest();
